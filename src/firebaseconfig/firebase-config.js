83736a82dfcc27af83c184fcc579b03b4c2f1be9
sakthivel <sakthivel.m2418@gmail.com>
2021-11-05 08:52:55 +0530

    build

--- src/firebaseconfig/firebase-config.js
+++ src/firebaseconfig/firebase-config.js
@@ -3,17 +3,6 @@ import {getAuth} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 
-  
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAWi0RUkh2i_RYUXu9vKCCz0x2etF0reyA",
-//   authDomain: "fitness-tracker-2b5c7.firebaseapp.com",
-//   projectId: "fitness-tracker-2b5c7",
-//   storageBucket: "fitness-tracker-2b5c7.appspot.com",
-//   messagingSenderId: "411817545639",
-//   appId: "1:411817545639:web:6066346846d2d13a4df082",
-//   measurementId: "G-60GSKZFQ4T"
-// };
-
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
